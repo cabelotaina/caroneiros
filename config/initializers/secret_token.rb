9871fc91e5b2adbf70ab1932ed15712c7b1f25ea
Maurilio Atila <cabelotaina@gmail.com>
2014-02-01 00:16:55 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiCarona::Application.config.secret_key_base = '89819e468bccdb1fb8368653e93b157dc91bd6b59c7a933c9c2a2098ebf8cb6a543fe90adadf9ed32482845bf347343b705b0c9be5468ee2861f032dbdec98e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApiCarona::Application.config.session_store :cookie_store, key: '_api-carona_session'
