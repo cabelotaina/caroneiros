ccbd77df08b07e74e643e618f949a0a7449ab781
Maurilio Atila <cabelotaina@gmail.com>
2014-02-05 21:06:39 -0200

    login function ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   usar_como_cpf :cpf
+  has_many :rides, dependent: :destroy
 end
